76d8f4a8ce22addb735b4e60a8ea325a58910d7b
LisGC <lisjaviera@gmail.com>
2024-10-23 22:59:26 -0300

    algunos avances
    relizar mejor el cambio de texto que el movimiento

---
+++ TopSpin/Assets/Scripts/ListaCirculos.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListaCirculos : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> list;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
