fcc88052f346307e50ca04346811ff61494ffda3
LisGC <lisjaviera@gmail.com>
2024-11-04 22:20:43 -0300

    cosas movimiento derecha izquierda
    no toma el boton izquierdo

---
+++ TopSpin/Assets/Scripts/MovHaciaDerecha.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class MovHaciaDerecha : MonoBehaviour
+{
+    [SerializeField] public List<TextMeshPro> list;
+    private CheckWin checkWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkWin = FindAnyObjectByType<CheckWin>();
+    }
+
+    public void MoverDerecha()
+    {
+        if (list.Count > 1)
+        {
+            string ultimoNum = list[list.Count - 1].text;
+
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                list[i].text = list[i - 1].text;
+            }
+
+            list[0].text = ultimoNum;
+
+        }
+    }
+}
---
+++ TopSpin/Assets/Scripts/MovHaciaIzquierda.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine;
+
+public class MovHaciaIzquierda : MonoBehaviour
+{
+    [SerializeField] public List<TextMeshPro> list;
+    private CheckWin checkWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkWin = FindAnyObjectByType<CheckWin>();
+    }
+
+    public void MoverIzquierda()
+    {
+        if (list.Count > 1)
+        {
+            string primerNum = list[0].text;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                list[i].text = list[i + 1].text;
+            }
+
+            list[list.Count - 1].text = primerNum;
+        }
+    }
+
+    public void prueba()
+    {
+        Debug.Log("hoa");
+    }
+}
